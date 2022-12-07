27219ebb0fcedd5feceebaf48da5de1c1c7cd952
ArchiTECH ArchiTECH <davtian.garik@yandex.ru>
2022-12-07 14:14:27 +0300

    Домашнее задание по блоку 1.3.3

---
+++ homeWork_1.3.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MyMath;
+
+namespace homeWork_1._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            _3D_Vector myVector = new _3D_Vector(1, 5, 0);
+            myVector.Add_3D_Vector(4, 0, 6);
+
+            myVector.Dev_3D_Vector(2);
+
+            _3D_Vector myVector2 = new _3D_Vector(0, 2.5, 3.4);
+
+            myVector.Add_3D_Vector(ref myVector2);
+        }
+    }
+}
