fd394d645cefda2eef14d6d83fe8c14c4b4dc5ee
ArchiTECH ArchiTECH <davtian.garik@yandex.ru>
2022-12-07 16:47:09 +0300

    Все выводы в печать убраны из математических методов за ненадобностью. Добавлен метод вывода в печать

--- homeWork_1.3.3/3D_Vector.cs
+++ homeWork_1.3.3/3D_Vector.cs
@@ -12,73 +12,52 @@ namespace MyMath
 
         public _3D_Vector(double x, double y, double z)
         {
-            Console.WriteLine($"_3D_Vector CTor with args {{ {x}, {y}, {z}}} \n");
             _x = x; _y = y; _z = z;
+        }
+
+        public void Print_3D_Vector()
+        {
+            Console.WriteLine($"_3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
         }
 
         public void Add_3D_Vector(ref _3D_Vector other)
         {
-            Console.WriteLine("Using method { \"void Add_3D_Vector(ref _3D_Vector other)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
             _x += other._x;
             _y += other._y;
             _z += other._z;
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
         }
 
         public void Add_3D_Vector(double x, double y, double z)
         {
-            Console.WriteLine("Using method { \"void Add_3D_Vector(double x, double y, double z)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
             _x += x;
             _y += y;
             _z += z;
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
         }
 
         public void Sub_3D_Vector(ref _3D_Vector other)
         {
-            Console.WriteLine("Using method { \"void Sub_3D_Vector(ref _3D_Vector other)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
             _x -= other._x;
             _y -= other._y;
             _z -= other._z;
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
         }
 
         public void Sub_3D_Vector(double x, double y, double z)
         {
-            Console.WriteLine("Using method { \"void Sub_3D_Vector(double x, double y, double z)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
             _x -= x;
             _y -= y;
             _z -= z;
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
         }
 
         public void Mul_3D_Vector(double scalar)
         {
-            Console.WriteLine("Using method { \"void Mul_3D_Vector(double scalar)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
             _x *= scalar;
             _y *= scalar;
             _z *= scalar;
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
         }
 
         public void Dev_3D_Vector(double scalar)
         {
-            Console.WriteLine("Using method { \"void Dev_3D_Vector(double scalar)\" }");
-            Console.WriteLine($"On _3D_Vector with coords {{ {_x}, {_y}, {_z} }}");
-            // Было бы логично вызвать умножение на скаляр от значение 1 / scalar
-            // Mul_3D_Vector(1 / scalar);
-            // Но чтобы удовлетворить заданию по выводу результатов в консоль сделаем иначе
-
-            _x *= 1 / scalar;
-            _y *= 1 / scalar;
-            _z *= 1 / scalar;
-
-            Console.WriteLine($"Result _3D_Vector with coords {{ {_x}, {_y}, {_z} }} \n");
+            Mul_3D_Vector(1 / scalar);
         }
     }
 }
--- homeWork_1.3.3/Program.cs
+++ homeWork_1.3.3/Program.cs
@@ -9,16 +9,21 @@ namespace homeWork_1._3._3
         {
             _3D_Vector myVector = new _3D_Vector(1, 5, 0);
             myVector.Add_3D_Vector(4, 0, 6);          // прибавляем вектор из чисел
+            myVector.Print_3D_Vector();
 
             myVector.Dev_3D_Vector(2);                // делим вектор на скаляр - масштабируем - уменьшаем в два раза
+            myVector.Print_3D_Vector();
 
             _3D_Vector myVector2 = new _3D_Vector(0, 2.5, 3.4);
 
             myVector.Add_3D_Vector(ref myVector2);    // прибавляем другой вектор
+            myVector.Print_3D_Vector();
 
             myVector2.Sub_3D_Vector(1, 0.5, 0.4);     // вычитаем вектор из чисел
+            myVector2.Print_3D_Vector();
 
             myVector2.Mul_3D_Vector(2);               // умножаем вектор на скаляр - масштабируем - увеличиваем в два раза
+            myVector2.Print_3D_Vector();
         }
     }
 }
