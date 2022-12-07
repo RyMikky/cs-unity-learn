5add179a265634ca2a114acf0a6091cfb16739ab
ArchiTECH ArchiTECH <davtian.garik@yandex.ru>
2022-12-07 14:18:21 +0300

    Домашнее задание по модулю 1.3.3

--- homeWork_1.3.3/Program.cs
+++ homeWork_1.3.3/Program.cs
@@ -8,13 +8,17 @@ namespace homeWork_1._3._3
         static void Main(string[] args)
         {
             _3D_Vector myVector = new _3D_Vector(1, 5, 0);
-            myVector.Add_3D_Vector(4, 0, 6);
+            myVector.Add_3D_Vector(4, 0, 6);          // прибавляем вектор из чисел
 
-            myVector.Dev_3D_Vector(2);
+            myVector.Dev_3D_Vector(2);                // делим вектор на скаляр - масштабируем - уменьшаем в два раза
 
             _3D_Vector myVector2 = new _3D_Vector(0, 2.5, 3.4);
 
-            myVector.Add_3D_Vector(ref myVector2);
+            myVector.Add_3D_Vector(ref myVector2);    // прибавляем другой вектор
+
+            myVector2.Sub_3D_Vector(1, 0.5, 0.4);     // вычитаем вектор из чисел
+
+            myVector2.Mul_3D_Vector(2);               // умножаем вектор на скаляр - масштабируем - увеличиваем в два раза
         }
     }
 }
